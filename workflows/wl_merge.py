410896bd3fc58f8c8301aee5ccfa8b0f4e142394
MeiShinWu <MacyL@users.noreply.github.com>
2020-06-30 16:13:00 +0200

    add workflows

--- workflows/wl_merge.py
+++ workflows/wl_merge.py
@@ -2,9 +2,13 @@ from lingpy import *
 from lingpy import Wordlist
 from lexibank_chenhmongmien import Dataset as chenhmongmien
 from lexibank_ratliffhmongmien import Dataset as ratliffhmongmien
+from lexibank_wold import Dataset as wold
 from pyconcepticon import Concepticon
 from cldfcatalog import Config
-
+'''
+This script merged chen and wold data, and add ratliff's cognates as references
+'''
+# set up structure
 columns = [
     "local_id",
     "id",
@@ -35,16 +39,6 @@ namespace = {
     "parameter_id": "concepticon_id",
     "cogid_cognateset_id": "cogid",
 }
-ratliff = Wordlist.from_cldf(
-    ratliffhmongmien().cldf_dir.joinpath("cldf-metadata.json").as_posix(),
-    columns=columns,
-    namespace=namespace,
-)
-chen = Wordlist.from_cldf(
-    chenhmongmien().cldf_dir.joinpath("cldf-metadata.json").as_posix(),
-    columns=columns,
-    namespace=namespace,
-)
 
 concepticon = Concepticon(Config.from_file().get_clone("concepticon"))
 concepts_ref = {}
@@ -52,28 +46,65 @@ for concept in concepticon.conceptlists["Chen-2012-888"].concepts.values():
     if concept.concepticon_id:
         concepts_ref[concept.concepticon_gloss] = concept.concepticon_id
 
+languages = {
+    row[0]: [row[3], row[4]]
+    for row in csv2list("../languages.tsv", strip_lines=False)[1:]
+}
+# load datasets
+ratliff = Wordlist.from_cldf(
+    ratliffhmongmien().cldf_dir.joinpath("cldf-metadata.json").as_posix(),
+    columns=columns,
+    namespace=namespace,
+    filter=lambda row: row['language_id'] in languages.keys()
+)
+chen = Wordlist.from_cldf(
+    chenhmongmien().cldf_dir.joinpath("cldf-metadata.json").as_posix(),
+    columns=columns,
+    namespace=namespace,
+    filter=lambda row: row['language_id'] in languages.keys()
+)
+
+whitehmong = Wordlist.from_cldf(
+    wold().cldf_dir.joinpath("cldf-metadata.json").as_posix(),
+    columns=columns,
+    namespace=namespace,
+    filter=lambda row: row['language_id'] == 'WhiteHmong'
+)
+
+# cognate id info from ratliff, need to confirm with Mattis
+ratliff.add_entries('classification', 'doculect', lambda x: languages[x][0])
+
+# merging
 concepts = set()
 for c in ratliff.concept:
     if c in concepts_ref.keys():
         concepts.add(concepts_ref[c])
 
-languages = {
-    row[0]: [row[3], row[4]]
-    for row in csv2list("../languages.tsv", strip_lines=False)[1:]
-}
-
-Combine = {0: [x for x in namespace.values()] + ["classification", "dataset"]}
+Combine = {0: [x for x in namespace.values()] + ["classification"]}
 entry_id = 1
-ds = [ratliff, chen]
+ds = [whitehmong, chen]
 for dataset in ds:
     for idx, c, l in dataset.iter_rows("concepticon", "doculect"):
         if c in concepts:
-            if l in languages:
-                Combine[entry_id] = []
-                tmp = [dataset[idx, x] for x in Combine[0][:13]]
-                tmp = tmp + languages.get(l, "?")
-                Combine[entry_id] = tmp
-                entry_id += 1
+            tmp = [dataset[idx, x] for x in Combine[0][:13]]+[languages[l][0]]
+            Combine[entry_id] = tmp
+            entry_id += 1
 
+# adding info
 Combine_wl = Wordlist(Combine)
+used_cogid=set()
+for idx, concept, language, classification in Combine_wl.iter_rows('concept', 'doculect', 'classification'):
+    for ridx in ratliff:
+        if concept==ratliff[ridx,'concept'] and classification == ratliff[ridx, 'classification']:
+            Combine_wl[idx,'cogid'] = ratliff[ridx, 'cogid']
+            used_cogid.add(ratliff[ridx, 'cogid'])
+Combine_wl.output('tsv', filename='HM-wordlist-for-evaluate', prettify=False)
+
+# fix cogid ==0
+min_cogid = max(used_cogid)+1
+for idx in Combine_wl:
+    if Combine_wl[idx, 'cogid']==0:
+        Combine_wl[idx, 'cogid']=min_cogid
+        min_cogid +=1
+
 Combine_wl.output("tsv", filename="HM-wordlist", prettify=False)
--- workflows/wl_partial.py
+++ workflows/wl_partial.py
@@ -1,10 +1,12 @@
 from lingpy import *
 from lingpy.compare.partial import Partial
+from lebor.algorithm import cogids2cogid
 
+# partial cognate 
 try:
     part = Partial("HM-wordlist-partial.bin.tsv")
 except:
-    part = Partial("HM-wordlist.tsv", segments="tokens")
+    part = Partial("HM-wordlist-for-evaluate.tsv", segments="tokens")
     part.get_partial_scorer(runs=10000)
     part.output("tsv", filename="HM-wordlist.bin", ignore=[], prettify=False)
 finally:
@@ -16,5 +18,9 @@ finally:
         gop=-2,
         cluster_method="infomap",
     )
+
+# convert to full cognate
+cogids2cogid(part, ref='cogids', cognates='lebor_cogid')
+
 
 part.output("tsv", filename="HM-wordlist-partial", prettify=False)
