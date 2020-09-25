333768afc324b66a713d18a16d7b5b0449a670be
MeiShinWu <MacyL@users.noreply.github.com>
2020-09-25 14:20:13 +0200

    update

--- pkg/networks.py
+++ pkg/networks.py
@@ -1,14 +1,11 @@
 from lingpy import *
-from lingpy.convert.strings import matrix2dst, write_nexus
+
 from util import base_path
 
 wl = Wordlist(base_path.joinpath("hmong-mein-analysis.tsv").as_posix())
-
-# output nexus
-for i in range(5,15):
-    col = 't'+str(i)
-    nexus = write_nexus(wl, 
-                        ref=col,
-                        mode='SPLITSTREE',
-                        filename=''.join(['HM',col,'.nex'])
-                        )
+# reference  : cogid
+wl.calculate("dst", taxa="doculect", ref="cogid")
+wl.output("dst", filename="hm-cogid", taxa="doculect", ref="cogid")
+# threshold 0.55, cogids2cogid
+wl.calculate("dst", taxa="doculect", ref="autot11")
+wl.output("dst", filename="hm-autot11", taxa="doculect", ref="autot11")
