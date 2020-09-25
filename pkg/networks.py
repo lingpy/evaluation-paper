11ea0308d9d8674a2fc3dc186dc000a2a7ed57e2
MeiShinWu <MacyL@users.noreply.github.com>
2020-09-25 15:31:20 +0200

    update netowrk.py

--- pkg/networks.py
+++ pkg/networks.py
@@ -5,7 +5,17 @@ from util import base_path
 wl = Wordlist(base_path.joinpath("hmong-mein-analysis.tsv").as_posix())
 # reference  : cogid
 wl.calculate("dst", taxa="doculect", ref="cogid")
-wl.output("dst", filename="hm-cogid", taxa="doculect", ref="cogid")
+wl.output(
+    "dst",
+    filename=base_path.joinpath("hm-cogid").as_posix(),
+    taxa="doculect",
+    ref="cogid",
+)
 # threshold 0.55, cogids2cogid
 wl.calculate("dst", taxa="doculect", ref="autot11")
-wl.output("dst", filename="hm-autot11", taxa="doculect", ref="autot11")
+wl.output(
+    "dst",
+    filename=base_path.joinpath("hm-autot11").as_posix(),
+    taxa="doculect",
+    ref="autot11",
+)
