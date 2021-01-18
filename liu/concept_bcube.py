ea94c3621bca224d6e5584583af2b8b517d5e0d0
MeiShinWu <MacyL@users.noreply.github.com>
2021-01-18 18:56:14 +0100

    check bcube per concept

---
+++ liu/concept_bcube.py
@@ -0,0 +1,16 @@
+from lingpy.evaluate.acd import _get_bcubed_score as bcs
+from lingpy import Wordlist
+
+wordlist = Wordlist("liusinitic.tsv")
+
+ranks = []
+for concept in wordlist.rows:
+    idxs = wordlist.get_list(row=concept, flat=True)
+    cogsA = [wordlist[idx, "strictid"] for idx in idxs]
+    cogsB = [wordlist[idx, "looseid"] for idx in idxs]
+    p, r = bcs(cogsA, cogsB), bcs(cogsB, cogsA)
+    f = 2 * (p * r) / (p + r)
+    ranks += [[concept, p, r, f]]
+
+for concept, p, r, f in sorted(ranks, key=lambda x: x[-1]):
+    print("{0:20} | {1:.2f} | {2:.2f} | {3:.2f}".format(concept, p, r, f))
