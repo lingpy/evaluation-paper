67f2e30c3106458c3cf9e9f5e1f99edac120dde5
MeiShinWu <MacyL@users.noreply.github.com>
2021-01-18 16:36:14 +0100

    modify the code

--- liu/lexicostatistical.py
+++ liu/lexicostatistical.py
@@ -9,15 +9,14 @@ import pickle
 from lingpy import basictypes
 from clldutils.text import strip_brackets, split_text
 from collections import defaultdict
-import itertools
-
-"""
-function: auto cogid
-convert cogid from cogids
-"""
+from itertools import combinations
 
 
 def cogids2cogid(wordlist, ref="cogids", cognates="autoid", morphemes="morphemes_auto"):
+    """
+    function: auto cogid
+    convert cogid from cogids
+    """
     C, M = {}, {}
     current = 1
     for concept in wordlist.rows:
@@ -43,102 +42,83 @@ def cogids2cogid(wordlist, ref="cogids", cognates="autoid", morphemes="morphemes
         wordlist.add_entries(morphemes, M, lambda x: x)
 
 
-"""
-function: semi cogid
-convert cogid from cogids according to manually highlighted morpheme column
-"""
-
-
-def cogids2semi(wl, ref="cogids", cognates="semi", ref_morpheme="morphemes"):
-    # starting point
-    cids_array = set()
-    for idx, cids in part.iter_rows(ref):
-        for i in cids:
-            cids_array.add(i)
-    start_point = max(cids_array) + 10
-    # start converting
-    complex_cases = {}
-    for idx, concept, cids, mor in part.iter_rows("concept", ref, ref_morpheme):
-        if len(cids) == 1:
-            part[idx, cognates] = cids
+def cogid_from_morphemes(wl, ref="cogids", cognates="newcogid", morphemes="morphemes"):
+    """
+    function: semi cogid
+    convert cogid from cogids according to manually highlighted morpheme column
+    """
+    lookup, D = {}, {}  # store the data here
+    for idx, cogids, morphemes in wl.iter_rows("cogids", "morphemes"):
+        selected_cogids = []
+        for cogid, morpheme in zip(cogids, morphemes):  # make sure morphemes is a list!
+            if not morpheme.startswith("_"):
+                selected_cogids += [cogid]
+        salient = tuple(selected_cogids)
+        if salient in lookup:
+            D[idx] = lookup[salient]
+        elif D.values():
+            next_cogid = max(D.values()) + 1
+            lookup[salient] = next_cogid
+            D[idx] = next_cogid
         else:
-            hl = [i for i, x in enumerate(mor) if "_" not in x]
-            if len(hl) == 1:
-                part[idx, cognates] = cids[hl[0]]
-            else:
-                cids_tmp = sorted([cids[i] for i in hl])
-                cids_key = tuple(cids_tmp)
-                if cids_key not in complex_cases.keys():
-                    complex_cases[cids_key] = start_point
-                    part[idx, cognates] = start_point
-                    start_point += 1
-                else:
-                    part[idx, cognates] = complex_cases[cids_key]
+            lookup[salient] = 1
+            D[idx] = 1
+
+    wl.add_entries(cognates, D, lambda x: x)
 
 
-"""
-function: lexicostatistics
-This function ignore synonyms.
-"""
-
-
-def lexi_dist(wl, ctype, lanA, lanB):
-    common_concepts = 0
-    matches = 0
-    for c in part.concepts:
-        tmp = wl.get_dict(row=c, entry=ctype)
-        if len(tmp.get(lanA)) != 0 and len(tmp.get(lanB)) != 0:
-            common_concepts += 1
-        language_A, language_B = tmp[lanA], tmp[lanB]
-        intersect = [x for x in language_A if x in language_B]
-        if len(intersect) != 0:
-            matches += 1
-    dist = 1 - (matches / common_concepts)
-    return dist
+def lexical_distances(wl, ref="cogid"):
+    """
+    function: lexicostatistics
+    This function ignore synonyms.
+    """
+    matrix = [[0 for cell in range(wl.width)] for row in range(wl.width)]
+    for (i, langA), (j, langB) in combinations(enumerate(wl.cols), r=2):
+        common, total = 0, 0
+        lookupA, lookupB = wl.get_dict(col=langA, entry=ref), wl.get_dict(
+            col=langB, entry=ref
+        )
+        for concept in lookupA:
+            cogsA, cogsB = lookupA.get(concept, []), lookupB.get(concept, [])
+            if [x for x in cogsA if x in cogsB]:
+                common += 1
+                total += 1
+            elif cogsA and cogsB:  # this determines missing conepts now!
+                total += 1
+        matrix[i][j] = matrix[j][i] = 1 - (common / total)
+    return matrix
 
 
 """
 load data and recompute.
+to do : uncomment or delete the semiid part.
 """
 # part = Partial("liusinitic.tsv")
 part = Partial("liusinitic_semi.tsv")
-part.add_entries("semiid", "cogids", lambda x: 0)
+# part.add_entries("semiid", "cogids", lambda x: 0)
 part.add_cognate_ids("cogids", "strictid", idtype="strict")
 part.add_cognate_ids("cogids", "looseid", idtype="loose")
 cogids2cogid(part, ref="cogids", cognates="autoid", morphemes="morphemes_auto")
-cogids2semi(part, ref="cogids", cognates="semiid", ref_morpheme="morphemes")
-
+# cogid_from_morphemes(part, ref="cogids", cognates="semiid", morphemes="morphemes")
 
 """
 main task.
 """
-language_pair = {}
-for a, b in itertools.combinations(part.doculect, 2):
-    auto_d = lexi_dist(part, "autoid", a, b)
-    loose_d = lexi_dist(part, "looseid", a, b)
-    strict_d = lexi_dist(part, "strictid", a, b)
-    semi_d = lexi_dist(part, "semiid", a, b)
-    language_pair[(a, b)] = {
-        "autoid_dist": auto_d,
-        "strictid_dist": strict_d,
-        "looseid_dist": loose_d,
-        "semiid_dist": semi_d,
-    }
+Distances = {}
+for cognate in ["autoid", "looseid", "strictid", "semiid"]:
+    key = cognate + "_dist"
+    value = lexical_distances(part, cognate)
+    Distances[key] = value
+
 
 """
 output
 """
-doculect_number = len(part.doculect)
+doculect_number = part.width
 for output_column in ["autoid", "strictid", "looseid", "semiid"]:
     with open("lexi_{0}.dst".format(output_column), "w") as f:
         f.write("\t" + str(doculect_number) + "\n")
-        for i in part.doculect:
-            tmp = [i]
-            for j in part.doculect:
-                if language_pair.get((i, j)):
-                    tmp.append(str(language_pair[(i, j)][output_column + "_dist"]))
-                elif i == j:
-                    tmp.append(str(0))
-                else:
-                    tmp.append(str(language_pair[(j, i)][output_column + "_dist"]))
-            f.write("\t".join(tmp) + "\n")
+        m = Distances[output_column + "_dist"]
+        for i, doc in enumerate(part.cols):
+            tmp = "\t".join([str(x) for x in m[i]])
+            f.write("{0}\t{1}\n".format(doc, tmp))
