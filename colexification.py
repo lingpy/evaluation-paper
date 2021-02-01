1517ebe1d8ab432d7fc115ab4bf15d4d10ef804b
MeiShinWu <MacyL@users.noreply.github.com>
2021-02-01 12:20:19 +0100

    format it nicely

--- colexification.py
+++ colexification.py
@@ -10,7 +10,8 @@ from collections import defaultdict
 import statistics
 from tabulate import tabulate
 
-def colidx(wordlist, ref='cogids', concept='concept'):
+
+def colidx(wordlist, ref="cogids", concept="concept"):
 
     etd = wordlist.get_etymdict(ref=ref)
 
@@ -19,21 +20,19 @@ def colidx(wordlist, ref='cogids', concept='concept'):
         for cogid, reflexes in etd.items():
             if reflexes[i]:
                 concepts = [wordlist[idx, concept] for idx in reflexes[i]]
-                indices[ln][cogid] = len(set(concepts))-1
+                indices[ln][cogid] = len(set(concepts)) - 1
     all_scores = []
     for cnc in wordlist.rows:
         reflexes = wordlist.get_list(row=cnc, flat=True)
         scores = []
         for idx in reflexes:
-            ln, cogids = wordlist[idx, 'doculect'], wordlist[idx, ref]
-            scores += [
-                    statistics.mean([indices[ln][cogid] for cogid in cogids])]
+            ln, cogids = wordlist[idx, "doculect"], wordlist[idx, ref]
+            scores += [statistics.mean([indices[ln][cogid] for cogid in cogids])]
         all_scores += [[cnc, statistics.mean(scores)]]
     return sorted(all_scores, key=lambda x: (x[1], x[0]))
 
 
-wl = Wordlist('liusinitic.tsv')
+wl = Wordlist("liusinitic.tsv")
 
 scores = colidx(wl)
 print(tabulate(scores))
-
