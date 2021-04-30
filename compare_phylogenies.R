1e0a6abbd066ce53efc43ef4e27a928d0a776fed
MacyL <wumeishin@gmail.com>
2021-04-30 15:01:48 +0200

    modify the script

--- compare_phylogenies.R
+++ compare_phylogenies.R
@@ -1,35 +1,24 @@
 library("groundhog")
 groundhog.day="2021-04-21"
-groundhog.library(c('ape', 'maps'), groundhog.day)
-source('dependencies/mad.R') # source the mad.R from the file
-
-# Load taxa annotation
-tdf = read.csv('taxa-annotation.csv')
-tdf$Newlabel<-apply(tdf, 1,function(x) paste0(x[1], ' [',x[4],']'))
+groundhog.library('ape', groundhog.day)
 
 # Load the newick trees and change the tip labels
-loose = read.tree(file='results/looseid.nwk')
-strict = read.tree(file='results/strictid.nwk')
+loose = read.tree(file='results/part_loose.tre')
+strict = read.tree(file='results/part_strict.tre')
 Loose = unroot(loose)
 Strict = unroot(strict)
-Loose$tip.label= tdf[[5]][match(Loose$tip.label, tdf[[1]])]
-Strict$tip.label= tdf[[5]][match(Strict$tip.label, tdf[[1]])]
+
 
 # Compare unrooted Neighbor, save as a png
-pdf(file="plot/loose-strict-unroot.pdf",width = 15, height = 7)
+pdf(file="plots/loose-strict-unroot.pdf",width = 20, height = 8)
 comparePhylo(Loose, Strict, force.rooted=F, plot=T) #first figure
 dev.off()
 
 # MAD root
-Loose_mad = as.phylo(mad(Loose, 'full')[[6]][[1]])
-Strict_mad = as.phylo(mad(Strict, 'full')[[6]][[1]])
-Loose_mad$tip.label = replace(Loose_mad$tip.label, Loose_mad$tip.label == "@#Nanjing [M]@#", "Nanjing [M]")
-
-# replace the original Loose and Strict
-Loose = Loose_mad
-Strict = Strict_mad
+Loose_mad = read.tree(file='results/part_loose.tre.rooted')
+Strict_mad = read.tree(file='results/part_strict.tre.rooted')
 
 # Compare MADrooted trees
-pdf(file="plot/loose-strict-root.pdf",width = 20, height = 15)
-comparePhylo(Loose, Strict, force.rooted=F, plot=T) # second figure
+pdf(file="plots/loose-strict-root.pdf",width = 20, height = 15)
+comparePhylo(Loose_mad, Strict_mad, force.rooted=F, plot=T) # second figure
 dev.off()
