169c35f685f4a82c463c7678fcc31229efc868e5
MacyL <wumeishin@gmail.com>
2021-11-26 13:50:18 +0100

    add a compound type calculation

--- TTR/ttr.py
+++ TTR/ttr.py
@@ -28,10 +28,11 @@ wl = fetch(
 )
 
 morpheme_dict = {}
+compound_type_dict = {}
 compound_dict = {}
 character_dict = {}
-for idx, morphemes, compounds, characters in wl.iter_rows(
-    "morphemes", "compounds", "characters"
+for idx, concept, morphemes, compounds, characters in wl.iter_rows(
+    "concept", "morphemes", "compounds", "characters"
 ):
     for m in morphemes:
         tmp = m.split("/")[0]
@@ -40,6 +41,10 @@ for idx, morphemes, compounds, characters in wl.iter_rows(
         else:
             morpheme_dict[tmp] = 1
     compound = compounds.split(" ")
+    if compound_type_dict.get(compounds):
+        compound_type_dict[compounds].append((concept, characters.replace(" ", "")))
+    else:
+        compound_type_dict[compounds] = [(concept, characters.replace(" ", ""))]
     for c in compound:
         if compound_dict.get(c):
             compound_dict[c] += 1
@@ -59,6 +64,27 @@ for k, v in compound_dict.items():
     output_array.append([k, v])
 
 print(tabulate(output_array, headers=["Part of speech", "Times"]))
+
+# Compound type
+compound_array = []
+for k, v in compound_type_dict.items():
+    tmp_concepts = set()
+    tmp_characters = set()
+    for v1, v2 in v:
+        tmp_concepts.add(v1)
+        tmp_characters.add(v2)
+    compound_array.append(
+        [k, ",".join([x for x in tmp_concepts]), ",".join([y for y in tmp_characters])]
+    )
+
+with open("compound_type_summary.tsv", "w") as f:
+    f.write("|{0}|{1}|{2}|\n".format("Compound type", "Concepts", "Chinese"))
+    for each in compound_array:
+        f.write("|{0}|{1}|{2}|\n".format(each[0], each[1], each[2]))
+f.close()
+
+print(tabulate(compound_array, headers=["Compound type", "Concepts", "Chinese"]))
+
 
 # Type-Token Ratio: morpheme
 # ttr_morpheme = len(morpheme_dict.keys()) / sum(morpheme_dict.values())
